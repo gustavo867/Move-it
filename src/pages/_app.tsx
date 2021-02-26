71dde48c094bdb2d9cb59cc8dc53b6c9d9f6f251
gustavo867 <tartatavo3@gmail.com>
2021-02-26 00:34:59 -0300

    :sparkles: pages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ChallengesProvider } from "../context/ChallengeContext";
+import { CountDownProvider } from "../context/CountDownContext";
+import "../styles/global.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChallengesProvider>
+      <CountDownProvider>
+        <Component {...pageProps} />
+      </CountDownProvider>
+    </ChallengesProvider>
+  );
+}
+
+export default MyApp;
